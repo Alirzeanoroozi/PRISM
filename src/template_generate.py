94729f4150daa6589b7017f7dfce4f752ccf624a
Alirzeanoroozi <noroozi.alirezaa@gmail.com>
2026-02-23 21:10:03 +0300

    remove the pdb_bfactor and multi-core and cpu-only run

--- src/template_generate.py
+++ src/template_generate.py
@@ -1,28 +1,37 @@
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from .hotspot import hotspot_creator
 from .interface import generate_interface
 from .contact import get_contacts
 
+def process_template(template):
+    try:
+        generate_interface(template)
+        hotspot_creator(template)
+        get_contacts(template)
+        return template, None
+    except Exception as e:
+        return None, f"Error in template generation for {template}: {e}"
+
 def template_generator():
     calculated_templates = []
+
     with open("templates/checked_templates.txt", "r") as f:
         templates = [line.strip() for line in f.readlines()]
-    for template in tqdm(templates):
-        try:
-            # print(f"Interface Generation for {template} started!!")
-            generate_interface(template)
-            # print(f"Interface Generation for {template} Finished!!")
-            # print(f"HotSpot Generation for {template} Started!!")
-            hotspot_creator(template)
-            # print(f"HotSpot Generation for {template} Finished!!")
-            # print(f"Contact Generation for {template} Started!!")
-            get_contacts(template)
-            # print(f"Contact Generation for {template} Finished!!")
-            calculated_templates.append(template)
-        except Exception as e:
-            print(f"Error in template generation for {template}: {e}")
-            continue
+
+    futures = []
+    with ThreadPoolExecutor() as executor:
+        for template in templates:
+            futures.append(executor.submit(process_template, template))
+
+        for future in tqdm(as_completed(futures), total=len(templates)):
+            result, error = future.result()
+            if result is not None:
+                calculated_templates.append(result)
+            if error:
+                print(error)
+
     with open("templates/calculated_templates.txt", "w") as f:
         for template in calculated_templates:
             f.write(f"{template}\n")
